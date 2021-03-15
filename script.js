c514de8ead4549affe8df70e4fbc02a35d854ae9
Gabriel Dias <gabriel.menezes.dias@hotmail.com>
2021-03-15 18:02:18 -0300

    requisito 7

--- script.js
+++ script.js
@@ -14,4 +14,17 @@ function addItems() {
     clearInput();
   }
 }
+
+function selectItem() {
+  let notSelected = document.getElementsByTagName('li');
+  let gray = 'rgb(128, 128, 128)';
+  for(let index = 0; index < notSelected.length; index += 1) {
+    notSelected.target.classList.add('selected');
+  }
+  if(notSelected.classList.contains('selected')) {
+    this.style.backgroundColor = gray;
+  }
+}
+
 document.getElementById('criar-tarefa').addEventListener('click', addItems);
+document.getElementsByTagName('li').addEventListener('click', selectItem);
